09feccfef8b9be023a144674182e64d36889fa44
qw1te0v <111095553+qw1te0v@users.noreply.github.com>
2025-03-23 14:54:14 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,31 @@
+from aiohttp import web
 import asyncio
 from aiogram import Bot, Dispatcher
-from aiohttp import web
-
 from app.handlers import router
 
-# Telegram Bot токен
-API_TOKEN = '8062754523:AAFD-voY39Qmo9450AqeAJWLyNO0X9z6A34'
-
-# Создание aiohttp веб-сервера
 async def handle(request):
-    return web.Response(text="Bot is alive!")
+    return web.Response(text="Bot is running!")
 
 async def main():
-    bot = Bot(token=API_TOKEN)
+    bot = Bot(token='ТВОЙ_ТОКЕН')
     dp = Dispatcher()
     dp.include_router(router)
 
-    # Создание aiohttp приложения
+    # aiohttp server
     app = web.Application()
-    app.router.add_get('/', handle)  # Для проверки UptimeRobot
+    app.router.add_get("/", handle)  # отвечает UptimeRobot
     
-    # Запускаем Telegram Bot в отдельной задаче
-    loop = asyncio.get_running_loop()
-    loop.create_task(dp.start_polling(bot))
-
-    # Запускаем веб-сервер
     runner = web.AppRunner(app)
     await runner.setup()
     site = web.TCPSite(runner, port=8080)
     await site.start()
     
-    print("Bot and web server started...")
+    # запускаем бота
+    loop = asyncio.get_running_loop()
+    loop.create_task(dp.start_polling(bot))
 
-    # Чтобы приложение не завершалось
+    print("Bot and server started!")
+
     await asyncio.Event().wait()
 
 if __name__ == '__main__':
